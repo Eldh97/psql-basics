69699197fcf7a234bcdb68325932d4237a76bd56
eldh97 <eldh97@outlook.com>
2019-11-23 22:31:36 +0300

    Add more queries

--- connect.py
+++ connect.py
@@ -5,6 +5,7 @@ connection = psycopg2.connect('dbname=learningsql', user="postgres", password="l
 # Open a cursor to perform database operations
 cursor = connection.cursor()
 
+cursor.execute('DROP TABLE IF EXISTS table1;')
 
 cursor.execute('''
     CREATE TABLE table1(
@@ -15,7 +16,16 @@ cursor.execute('''
 )
 
 
-cursor.execute('INSERT INTO table1 (id, completed) VALUES (1, true);')
+cursor.execute('INSERT INTO table1 (id, completed) VALUES (%s, %s);',(3, True))
+
+SQL = 'INSERT INTO table1 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+ 'id':2,
+    'completed': False
+}
+cursor.execute(SQL, data)
+
 
 # commit, so it does the executions on the db and persists in the db
 connection.commit()
